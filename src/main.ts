e2598cda07bec1f5a867b3c5d8acd00ff270de67
FatherRtd <79792471+FatherRtd@users.noreply.github.com>
2025-04-24 19:17:57 +0500

    graph

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import VNetworkGraph from "v-network-graph";
+import "v-network-graph/lib/style.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,6 +13,7 @@ import router from "./router";
 const app = createApp(App);
 
 app.use(ElementPlus);
+app.use(VNetworkGraph);
 app.use(createPinia());
 app.use(router);
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,60 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import { reactive, ref } from "vue";
+import * as vNG from "v-network-graph";
+import data from "@/data";
+
+const layers = {
+  badge: "nodes",
+};
+
+const nodes = reactive({ ...data.nodes });
+
+const layouts = ref({ ...data.layouts });
+
+const edges = data.edges;
+const configs = data.configs;
+
+const eventHandlers: vNG.EventHandlers = {
+  "node:click": ({ node }) => {
+    nodes[node].active = !nodes[node].active;
+  },
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-network-graph
+    class="graph"
+    :nodes="nodes"
+    :edges="edges"
+    :layouts="layouts"
+    :configs="configs"
+    :layers="layers"
+    :event-handlers="eventHandlers"
+  >
+    <!-- Additional layer -->
+    <template #badge="{ scale }">
+      <!--
+        If the `view.scalingObjects` config is `false`(default),
+        scaling does not change the display size of the nodes/edges.
+        The `scale` is passed as a scaling factor to implement
+        this behavior. -->
+      <circle
+        v-for="(pos, node) in layouts.nodes"
+        :key="node"
+        :cx="pos.x + 9 * scale"
+        :cy="pos.y - 9 * scale"
+        :r="4 * scale"
+        :fill="nodes[node].active ? '#00cc00' : '#ff5555'"
+        style="pointer-events: none"
+      />
+    </template>
+  </v-network-graph>
 </template>
+
+<style scoped>
+.graph {
+  width: 800px;
+  height: 600px;
+  border: 1px solid #000;
+}
+</style>
